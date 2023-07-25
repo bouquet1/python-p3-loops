d139bfceddbf027d1f9144b7aa5ac1a94756ebbf
bouquet1 <bukuleta@gmail.com>
2023-07-24 17:27:00 -0700

    passes test

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,45 @@
 #!/usr/bin/env python3
 
+#while loop version
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i >= 0:  # Change the condition to include 0 in the loop
+        if i == 0:
+            print("Happy New Year!")
+        else:
+            print(i)
+        i -= 1
+
+happy_new_year()
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = [num ** 2 for num in int_list]
+    return squared_list
+
+result = square_integers([1, 2, 3, 4, 5])
+print(result) #boyle deyince sonucu yazdirdim [1, 4, 9, 16, 25]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if (num %3 == 0 and num %5 == 0):
+            print("FizzBuzz")
+        elif num % 5 == 0:
+            print("Buzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        else:
+            print(num)
+fizzbuzz()
+
+# Python while loop syntax
+# i = 0
+# while i < 5:
+#     print("Looping!")
+#     i += 1
+
+
+# python for loop syntax
+# for i in range(10):
+#     print("Looping!")
+#     print(f"i is: {i}")
